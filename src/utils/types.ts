ac10328580882a5eb1899edc33fb7d4f4b9b96e2
ElPrudi <liampruditsch@gmail.com>
2025-03-11 16:36:58 +0100

    fix: lint

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,58 +1,61 @@
-import type FontConfig from "../classes/FontConfig"
-import type { PreparedFontOptionVariance } from "../types"
-
+import type FontConfig from '../classes/FontConfig';
+import type { PreparedFontOptionVariance } from '../types';
+/* eslint-disable security/detect-object-injection */
 interface TextProperties {
-  [index: string]: (string | number)[]
-  family: string[]
-  weight: (string | number)[]
-  style: string[]
+  [index: string]: (string | number)[];
+  family: string[];
+  weight: (string | number)[];
+  style: string[];
 }
 
-function getFromVariances<T>(fontConfig: FontConfig, key: keyof PreparedFontOptionVariance): T[] {
-  const uniqueStyles = new Set<T>()
-  const fontCount = fontConfig.fonts.length
+function getFromVariances<T>(
+  fontConfig: FontConfig,
+  key: keyof PreparedFontOptionVariance
+): T[] {
+  const uniqueStyles = new Set<T>();
+  const fontCount = fontConfig.fonts.length;
 
   for (let i = 0; i < fontCount; i++) {
-    const values = fontConfig.fonts[i].variances
+    const values = fontConfig.fonts[i].variances;
     for (let j = 0, valueCount = values.length; j < valueCount; j++) {
-      uniqueStyles.add(values[j][key] as T)
+      uniqueStyles.add(values[j][key] as T);
     }
   }
 
-  return Array.from(uniqueStyles).filter(Boolean)
+  return Array.from(uniqueStyles).filter(Boolean);
 }
 
 function unique<T>(array: T[]): T[] {
-  return Array.from(new Set(array)).filter(Boolean)
+  return Array.from(new Set(array)).filter(Boolean);
 }
 
 function capitalize(text: string): string {
-  return text ? `${text.charAt(0).toUpperCase()}${text.slice(1)}` : text
+  return text ? `${text.charAt(0).toUpperCase()}${text.slice(1)}` : text;
 }
 
 function generateAliases(textProperties: TextProperties): string {
-  const props = Object.keys(textProperties)
-  const len = props.length
-  const output: string[] = new Array(len)
+  const props = Object.keys(textProperties);
+  const len = props.length;
+  const output: string[] = new Array(len);
   // Loop through each text property
   for (let i = 0; i < len; i++) {
-    const prop = props[i]
-    const propValues: (string | number)[] = textProperties[props[i]]
-    const valueCount = propValues.length
-    let line = `export type Font${capitalize(prop)} = `
+    const prop = props[i];
+    const propValues: (string | number)[] = textProperties[props[i]];
+    const valueCount = propValues.length;
+    const line = `export type Font${capitalize(prop)} = `;
     // Output generic type "string", if there are no values to parse
     if (!valueCount) {
-      output[i] = `${line}'string';`
-      continue
+      output[i] = `${line}'string';`;
+      continue;
     }
     // Merge all values into a union (|) type value.
-    const values: string[] = new Array(valueCount)
+    const values: string[] = new Array(valueCount);
     for (let j = 0; j < valueCount; j++) {
-      values[j] = `'${propValues[j]}'`
+      values[j] = `'${propValues[j]}'`;
     }
-    output[i] = `${line}${values.join(' | ')};`
+    output[i] = `${line}${values.join(' | ')};`;
   }
-  return output.join('\n')
+  return output.join('\n');
 }
 
 export function getTypesContent(fontConfig: FontConfig): string {
@@ -60,5 +63,6 @@ export function getTypesContent(fontConfig: FontConfig): string {
     family: unique(fontConfig.fonts.map(font => font.family)),
     weight: getFromVariances(fontConfig, 'weight'),
     style: getFromVariances(fontConfig, 'style')
-  })
+  });
 }
+/* eslint-enable security/detect-object-injection */
